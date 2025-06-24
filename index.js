89cb6800b09fd28cca688268df6cfec58bbf155f
KarimLabidProf <karim.labidi@esprit.tn>
2025-06-24 15:54:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const http = require('http');
+// // app.js
+// const http = require("http");
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = "127.0.0.1";
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World!\n");
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+// index.js
+console.log("Hello from Jenkins!");
+const date = new Date();
+console.log("Current date and time is:", date.toString());
